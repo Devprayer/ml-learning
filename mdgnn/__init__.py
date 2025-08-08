b70a6eda95e2d1ff006648b69eba221011992614
Devprayer <89265424+Devprayer@users.noreply.github.com>
2025-08-08 19:50:57 +0800

    Delete mdgnn directory

--- mdgnn/__init__.py
+++
@@ -1,8 +0,0 @@
-from .model import MDGNN, MultiRelationalGCNLayer
-from .data import generate_synthetic_stock_data, SyntheticStockDataset
-__all__ = [
-    "MDGNN",
-    "MultiRelationalGCNLayer",
-    "generate_synthetic_stock_data",
-    "SyntheticStockDataset",
-]
